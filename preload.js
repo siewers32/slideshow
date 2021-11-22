d73f37c176a2636c95eaf3069a657f8788caeffe
Jan Jaap <janjaap@siewers.org>
2021-11-22 21:51:15 +0100

    slideshow verbeterd

--- preload.js
+++ preload.js
@@ -1,31 +1,31 @@
 //const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
-var slideInterval = setInterval(transition, 1000);
+var slideInterval = setInterval(transition, 3000);
 var slideShow = document.getElementById('slideshow');
-var images = [];
 var divs = [];
+var transitionCount = 0;
 slideShow.addEventListener('click', openFullscreen);
 
 fetch('files.php', {mode: 'cors'})
     .then(function(response) {
         return response.json();
     })
-    .then(function(data) {
-        console.log('Request successful', data);
-        images = data;
-        addElements();
+    .then(function(images) {
+        console.log('Request successful', images);
+        addElements(images);
     })
     .catch(function(error) {
         console.log('Request failed', error)
     });
 
-function addElements() {
+function addElements(images) {
     for (var i = 0; i < images.length; i++) {
         let url = "images/"+images[i];
-        if(i == (images.length-1)) {
-            addElement(999, url, 0);
-        } else {
-            addElement(i, url, i);
-        }
+        addElement(i, url, i);
+        // if(i == (images.length-1)) {
+        //     addElement(999, url, 0);
+        // } else {
+        //     addElement(i, url, i);
+        // }
     }
 }
 
@@ -34,22 +34,34 @@ function addElement(id, url, zindex) {
     div.style.backgroundImage = "url(" + url + ")";
     div.style.zIndex = zindex;
     div.id = "slideshow-image" + id;
-    div.classList = "";
+    //div.style.display = "none";
+    //div.classList = "";
     div.className = "slideshow-image";
     slideShow.appendChild(div);
 }
 
+
+
 function transition() {
     divs = document.getElementsByClassName("slideshow-image");
-    if(divs.length <= 2) {
-        //clearInterval(slideInterval);
+    if(divs.length <= 0) {
+        clearInterval(slideInterval);
         addElements();
     } else {
-        divToBeDissolved = document.getElementsByClassName("slideshow-image") [divs.length - 2];
-        divToBeDissolved.classList.add('dissolve');
-        divToBeRemoved = document.getElementsByClassName("slideshow-image") [divs.length - 1];
-        console.log(divToBeRemoved);
-        divToBeRemoved.remove();
+        if(divs.length >= 2) {
+            if(transitionCount == 0) {
+                divToBeDissolved = divs[divs.length - 1];
+            } else {
+                divToBeDissolved = divs[divs.length - 2];
+            }
+            console.log(divToBeDissolved);
+            divToBeDissolved.classList.add('dissolve');
+        }
+        if(transitionCount > 0) {
+            divToBeRemoved = divs[divs.length - 1];
+            divToBeRemoved.remove();
+        }
+        transitionCount++;
     }
 }
 
--- rename.php
+++ rename.php
@@ -7,24 +7,27 @@ renameFiles($whitelist);
 function renameFiles($whitelist) {
     $dir = __DIR__."/images";
     $files = scandir($dir);
+    $counter = 0;
     foreach($files as $key => $filename) {
         $path = $dir."/".$filename;
         $extension = pathinfo($path, PATHINFO_EXTENSION);
         if(in_array($extension, $whitelist)) {
-            $newPath = $dir."/".generateName($key, $extension);
+            $counter++;
+            $newPath = $dir."/".generateName($counter, $extension, "test");
             echo $path.'--->'.$newPath;
             rename($path, $newPath);
         }
     }
 }
 
-function generateName($key, $extension) {
+function generateName($key, $extension, $name = "") {
     $counter = str_pad($key, 4, "0", STR_PAD_LEFT);
     $chars = [1,2,3,4,5,6,7,8,9,0,'a','b','c','d','e','f','g','h','i','j','k','l',
         'm','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
         'Q','R','S','T','U','V','W','X','Y','Z'];
     shuffle($chars);
-    return implode(array_slice($chars, 0, 8)).'_'.$counter.'.'.$extension;
+    //return $counter.'_'.implode(array_slice($chars, 0, 4)).'.'.$extension;
+    return $counter.'_'.$name.'.'.$extension;
 }
 
 
